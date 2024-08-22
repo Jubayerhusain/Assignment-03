a086bad67b4c2eca0e249188f7347d72463d0fa9
Jubayer Husain <jubayerhusain360@gmail.com>
2024-08-23 00:55:31 +0600

    Let’s Discuss Next Project -input-content

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/hero-pattern.svg')",
+          'footer-texture': "url('./assets/bg.jpg')",
+        }
+      }
+    }
+  }
